7c3f9af62a73de1e341f14e0d4091e98383f5c6f
CobaltHeroFT1128 <krysten.johnson.student@madisonpsb.org>
2026-01-22 21:20:00 +0000

    circularity project

--- fsd-projects/circularity/js/init.js
+++ fsd-projects/circularity/js/init.js
@@ -43,7 +43,9 @@ var init = function (window) {
 
 
         // TODO 7 : Use a loop to create multiple circles
-
+       for (var i = 0; i < 25; i++) {
+      drawCircle();
+}
 
 
 
@@ -65,9 +67,11 @@ var init = function (window) {
             physikz.updatePosition(circles[4]);
 
             // TODO 5 : Call game.checkCirclePosition() on your circles
-           
-
-
+            game.checkCirclePosition(circles[0]);
+            game.checkCirclePosition(circles[1]);
+            game.checkCirclePosition(circles[2]);
+            game.checkCirclePosition(circles[3]);
+            game.checkCirclePosition(circles[4]);
 
             // TODO 8 / TODO 9 : Iterate over the array
            
@@ -87,7 +91,23 @@ var init = function (window) {
             }
             
             // TODO 6 : YOUR CODE STARTS HERE //////////////////////
-            
+            if (circle.x > canvas.width){
+                circle.x = 0;
+            }
+            else if (circle.x < 0){
+                circle.x = canvas.width;
+            }
+            if (circle.y > canvas.height){
+                circle.y = 0;
+            }
+             else if (circle.y < 0){
+                circle.y = canvas.height;
+             }
+
+
+
+
+
 
 
             // YOUR TODO 6 CODE ENDS HERE //////////////////////////
