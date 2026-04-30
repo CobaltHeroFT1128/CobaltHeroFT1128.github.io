92e8d73bfcd5a6ca0b253b85b7d9da38b07421a4
CobaltHeroFT1128 <krysten.johnson.student@madisonpsb.org>
2026-04-30 19:18:34 +0000

    completed

--- asd-projects/data-shapes/index.js
+++ asd-projects/data-shapes/index.js
@@ -39,7 +39,8 @@ $(document).ready(function () {
    {color : "blue", 
    shape : "circle",
    repeat: 3
-   }
+   } 
+   dataShapes.push(shape)
   // TODO 2: add a new property to all data shapes
   for (var i = 0; i < dataShapes.length; i++){
     let shape = dataShapes[i];
